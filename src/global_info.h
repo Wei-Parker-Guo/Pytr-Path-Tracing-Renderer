621fda1d65b4ec135040ec9d30cdc979bca578ee
Wei Guo <wei.parker.guo.sg@gmail.com>
2020-08-22 21:31:51 +0800

    [FrontEnd] Integrated Ultralight html engine for the app.

---
+++ src/global_info.h
@@ -0,0 +1,32 @@
+#ifndef GLOBAL_INFO_H
+#define GLOBAL_INFO_H
+/*
+	GLOBAL INFO POOL FOR PYTR, DON'T EDIT
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string read_html_file(const char* file_name);
+
+//function to read a raw html string given a file
+string read_html_file(const char* file_name) {
+	ifstream file(file_name);
+	string content;
+	if (file.is_open()) {
+		string line;
+		while (getline(file, line)) {
+			content += (line + "\n");
+		}
+	}
+	file.close();
+	return content;
+}
+
+const char* VERSION_NAME = "Malenchenko(v1.0)";
+const char* INDEX_HTML = "index.html";
+
+
+#endif // !GLOBAL_INFO_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,35 @@
+#include <stdio.h>
+
+//global info
+#include "global_info.h"
+
+//UI Pipeline (Ultralight HTML Engine)
+#include <AppCore/App.h>
+#include <AppCore/Window.h>
+#include <AppCore/Overlay.h>
+#include <AppCore/Monitor.h>
+
 int main(int argc, char *argv[]){
-    return 42;
+    
+    using namespace ultralight;
+    //create main app
+    Ref<App> main_app = App::Create();
+    //create main window to fit as a fullscreen resolution size
+    Monitor* main_monitor = main_app->main_monitor();
+    Ref<Window> main_window = Window::Create(main_monitor, main_monitor->width() / main_monitor->scale(),
+        main_monitor->height() / main_monitor->scale(), false, kWindowFlags_Maximizable);
+    //set title of this window
+    char title_buf[64];
+    sprintf_s(title_buf, "Pytr Renderer [%s]", VERSION_NAME);
+    main_window->SetTitle(title_buf);
+    //set window to app
+    main_app->set_window(main_window);
+    ////set an overlay
+    Ref<Overlay> overlay = Overlay::Create(main_window, main_window->width(), main_window->height(), 0, 0);
+    ////specify overlay to to read from our homepage html
+    overlay->view()->LoadHTML(read_html_file(INDEX_HTML).data());
+    //run
+    main_app->Run();
+
+    return 0;
 }
