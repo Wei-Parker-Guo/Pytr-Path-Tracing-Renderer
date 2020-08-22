69252f5e4ae0045ff8be08afeca9c39f76609d7c
Wei Guo <wei.parker.guo.sg@gmail.com>
2020-08-23 05:26:16 +0800

    [Frontend] Added debug tools for the app.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,38 @@
 #include <AppCore/Window.h>
 #include <AppCore/Overlay.h>
 #include <AppCore/Monitor.h>
+#include <AppCore/JSHelpers.h>
+
+using namespace ultralight;
+
+//app instance
+class MyApp {
+    RefPtr<Overlay> overlay_;
+    RefPtr<Overlay> inspec_overlay;
+public:
+    MyApp(Ref<Window> win) {
+        ///
+        /// Create an Overlay using the same dimensions as our Window.
+        ///
+        overlay_ = Overlay::Create(win, win->width(), win->height(), 0, 0);
+
+        ///
+        /// Load a file from the FileSystem.
+        ///
+        ///  **IMPORTANT**: Make sure `file:///` has three (3) forward slashes.
+        ///
+        overlay_->view()->LoadURL("file:///index.html");
+
+        //uncomment this section to do webpage debugging
+        //RefPtr<View> inspector_view = overlay_->view()->inspector();
+        //inspector_view->Resize(win->width(), win->height() - 500);
+        ////create an inspector to debug
+        //inspec_overlay = Overlay::Create(win, *inspector_view.get(), 0, 500);
+        //inspec_overlay->Focus();
+    }
+
+    virtual ~MyApp() {}
+};
 
 int main(int argc, char *argv[]){
     
@@ -15,23 +47,20 @@ int main(int argc, char *argv[]){
     //APPLICATION SETUP
     //***********************************
 
-    using namespace ultralight;
     //create main app
-    Ref<App> main_app = App::Create();
+    auto main_app = App::Create();
     //create main window to fit as a fullscreen resolution size
     Monitor* main_monitor = main_app->main_monitor();
-    Ref<Window> main_window = Window::Create(main_monitor, main_monitor->width() / main_monitor->scale(),
-        main_monitor->height() / main_monitor->scale(), false, kWindowFlags_Maximizable);
+    auto main_window = Window::Create(main_monitor, main_monitor->width() / main_monitor->scale() / 1.3,
+        main_monitor->height() / main_monitor->scale() / 1.3, false, kWindowFlags_Titled|kWindowFlags_Resizable|kWindowFlags_Maximizable);
     //set title of this window
     char title_buf[64];
     sprintf_s(title_buf, "Pytr Renderer [%s]", VERSION_NAME);
     main_window->SetTitle(title_buf);
     //set window to app
     main_app->set_window(main_window);
-    ////set an overlay
-    Ref<Overlay> overlay = Overlay::Create(main_window, main_window->width(), main_window->height(), 0, 0);
-    ////specify overlay to to read from our homepage html
-    overlay->view()->LoadHTML(read_html_file(INDEX_HTML).data());
+    //create myapp instance
+    MyApp my_app(main_window);
 
 
     //***********************************
